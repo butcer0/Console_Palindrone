91eb011bf7d7e4d5b9ec69df66ec471210df0ab1
Erik Butcher <butcer0@gmail.com>
2020-05-15 23:15:01 -0400

    Updated PacManAI - Running competition timeout

--- ConsoleApp_Sandbox/AI Bots/PacMan/PacManAI.cs
+++ ConsoleApp_Sandbox/AI Bots/PacMan/PacManAI.cs
@@ -25,28 +25,23 @@ class Player
     static Dictionary<int, List<int[]>> Pellets = new Dictionary<int, List<int[]>>();
     static List<int> PartitionStatus = new List<int>();
 
-
     static void Main(string[] args)
     {
         string[] inputs;
         inputs = Console.ReadLine().Split(' ');
         int width = int.Parse(inputs[0]); // size of the grid
         int height = int.Parse(inputs[1]); // top left corner is (x=0, y=0)
+        
         PartitionMap = new int[height, width];
-
         for (int y = 0; y < height; y++)
         {
-            Map.Add(Console.ReadLine());  // one line of the grid: space " " is floor, pound "#" is wall
+             Map.Add(Console.ReadLine());  // one line of the grid: space " " is floor, pound "#" is wall
             
             var segmentWidth = width/3;
             for(var x = 0; x < width; x++) {
                 var subParX = x < segmentWidth ? 0: x < segmentWidth*2 ? 1:2; 
                 var subParY = y < height/2 ? 0:3;
                 var subPartition = subParX + subParY;
-                
-                // if(y >= 0) {
-                //     Console.Error.WriteLine(string.Format("SubPartition [{0},{1}]: {2}*{3}={4}", x, y, subParX, subParY, subPartition));
-                // }
                 
                 PartitionMap[y,x] = subPartition;
             }
@@ -60,12 +55,10 @@ class Player
         }
 
         var initialized = false;
-        
+
         // game loop
         while (true)
         {
-            // Console.Error.WriteLine("Starting game loop");
-
             inputs = Console.ReadLine().Split(' ');
             int myScore = int.Parse(inputs[0]);
             int opponentScore = int.Parse(inputs[1]);
@@ -80,9 +73,7 @@ class Player
                 string typeId = inputs[4]; // unused in wood leagues
                 int speedTurnsLeft = int.Parse(inputs[5]); // unused in wood leagues
                 int abilityCooldown = int.Parse(inputs[6]); // unused in wood leagues
-
-                // Console.Error.WriteLine(string.Format("Parsed input: [{0},{1}] {2})", x, y, mine?"mine":"enemey"));
-
+            
                 if(!initialized) {
                     if(mine) {
                         PacMen.Add(pacId, new int[3]{x, y, STATUS_AVAILABLE});
@@ -91,81 +82,77 @@ class Player
                         EnemyPacMen.Add(pacId, new int[2]{x, y});
                     }
                 } else {
-                    // Console.Error.WriteLine("Starting update PacMen location");
-                    // todo: reintroduce                   
-                    // if(mine) {
-                    //     PacMen[pacId][0] = x;
-                    //     PacMen[pacId][1] = y;
-                    // } else {
-                    //     EnemyPacMen[pacId][0] = x;
-                    //     EnemyPacMen[pacId][1] = y;
-                    // }
+                    if(mine) {
+                        PacMen[pacId][0] = x;
+                        PacMen[pacId][1] = y;
+                    } else {
+                        EnemyPacMen[pacId][0] = x;
+                        EnemyPacMen[pacId][1] = y;
+                    }
                 }
 
-                // Console.Error.WriteLine("Updated PacMen Locations");
-
-                if(mine & PacMen[pacId][2] == STATUS_AVAILABLE) {
+                 if(mine & PacMen[pacId][2] == STATUS_AVAILABLE) {
                     Console.Error.WriteLine(string.Format("Getting Partition for: {0}", pacId));
-                    PacMen[pacId][2] = getAvailablePartition();           
+                    PacMen[pacId][2] = getAvailablePartition();
                 }
 
-                // Console.Error.WriteLine("Updated PacMen Locations");
-
-                if(initialized) {
+                 if(initialized) {
                     // drop target if hit
-                    // todo: reintroduce
-                    // if(PacMenTargets[pacId] != null && PacMen[pacId][0] == PacMenTargets[pacId][0] && PacMen[pacId][1] == PacMenTargets[pacId][1]) {
-                    //     PacMenTargets[pacId] = null;
-                    // }
+                    if(mine && PacMenTargets[pacId] != null && PacMen[pacId][0] == PacMenTargets[pacId][0] && PacMen[pacId][1] == PacMenTargets[pacId][1]) {
+                        Console.Error.WriteLine(string.Format("Target Hit: [{0},{1}]-Id: {2}", PacMen[pacId][0],PacMen[pacId][1],pacId));
+                        PacMenTargets[pacId] = null;
+                    }
 
                     // set target if empty
                     // todo: reintroduce
-                    // if(PacMenTargets[pacId] == null) {
-                    //     PacMenTargets[pacId] = getClosestSubpartitionPellet(PacMen[pacId]);
-                    // } 
+                    if(mine && PacMenTargets[pacId] == null) {
+                        Console.Error.WriteLine("Getting Pellet "+pacId);
+                        PacMenTargets[pacId] = getClosestSubpartitionPellet(PacMen[pacId]);
+                        // var pelletFound = getClosestSubpartitionPellet(PacMen[pacId]);
+                        if(PacMenTargets[pacId] == null) {
+                            Console.Error.WriteLine(string.Format("Partition Complete: {0}", PacMen[pacId][2]));
+                            if(PacMen[pacId][2] >= 0) {
+                                PartitionStatus[PacMen[pacId][2]] = STATUS_COMPLETE;
+                            }
+                            PacMen[pacId][2] = STATUS_AVAILABLE;
+                        }
+                        Console.Error.WriteLine(string.Format("Closest Pellet Found: {0}", PacMenTargets[pacId] != null ? PacMenTargets[pacId][0].ToString():"Error"));
+                    } 
                 }
             }
-            
-            // Console.Error.WriteLine("Finished PacMen, Starting Pellets");
-
             int visiblePelletCount = int.Parse(Console.ReadLine()); // all pellets in sight
-            if(!initialized) {
-                var pelletPartition = 0;
-                for (int i = 0; i < visiblePelletCount; i++)
-                {
-                    inputs = Console.ReadLine().Split(' ');
-                    int x = int.Parse(inputs[0]);
-                    int y = int.Parse(inputs[1]);
-                    int value = int.Parse(inputs[2]); // amount of points this pellet is worth
-                    
-                    // todo: reintroduce
-                    // pelletPartition = PartitionMap[y,x];
+            var pelletPartition = 0;
+            for (int i = 0; i < visiblePelletCount; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int x = int.Parse(inputs[0]);
+                int y = int.Parse(inputs[1]);
+                int value = int.Parse(inputs[2]); // amount of points this pellet is worth
+            
+                if(!initialized){
+                    pelletPartition = PartitionMap[y,x];
                 
-                    // todo: reintroduce;
-                    // Pellets[pelletPartition].Add(new int[3]{x, y, value == 1 ? 1 : SUPER_PELLET_VALUE});
-                    // Console.Error.WriteLine(string.Format("Adding Pellet[{0}]: [{1},{2}]: {3}", pelletPartition, x, y, value));
+                    Pellets[pelletPartition].Add(new int[3]{x, y, value == 1 ? 1 : SUPER_PELLET_VALUE});
+                    Console.Error.WriteLine(string.Format("Adding Pellet[{0}]: [{1},{2}]: {3}", pelletPartition, x, y, value));
                 }
+                    
             }
             initialized = true;
-
-            Console.Error.WriteLine("Initialized & Pellets Complete");
-
-            // Console.WriteLine("MOVE 0 15 10"); // MOVE <pacId> <x> <y>
-            var action = string.Empty;
             
-            // todo: reintroduce
-            // foreach(var pacManTarget in PacMenTargets) {
-            //     if(pacManTarget.Value != null) {
-            //         action += string.Format("MOVE {0} {1} {2}|", pacManTarget.Key, pacManTarget.Value[0], pacManTarget.Value[1]);
-            //     }    
-            // }
+            var action = string.Empty;
+            foreach(var pacManTarget in PacMenTargets) {
+                if(pacManTarget.Value != null) {
+                    action += string.Format("MOVE {0} {1} {2}|", pacManTarget.Key, pacManTarget.Value[0], pacManTarget.Value[1]);
+                }    
+            }
             action = string.IsNullOrEmpty(action)? "MOVE 0 15 10" : action.TrimEnd('|');
             // Console.Error.WriteLine(string.Format("Sending Action: {0}", action));
             Console.WriteLine(action);
+
         }
     }
 
-    static int getAvailablePartition() {
+     static int getAvailablePartition() {
         for(var i = 0; i < NUM_PARTITIONS; i++) {
             if(PartitionStatus[i] == STATUS_AVAILABLE) {
                 PartitionStatus[i] = STATUS_BUSY;
@@ -177,7 +164,8 @@ class Player
 
     static int[] getClosestSubpartitionPellet(int[] pacMan) {
         // Console.Error.WriteLine(string.Format("Getting Pellet Location: [{0},{1},{2}]", pacMan[0],pacMan[1],pacMan[2]));
-        if(pacMan[2] < STATUS_AVAILABLE) {
+        
+        if(pacMan[2] == STATUS_AVAILABLE) {
             return null;
         }
         
@@ -187,12 +175,14 @@ class Player
         foreach(var pellet in Pellets[pacMan[2]]) {
             
             currentValue = getLocationValue(pacMan, pellet);
-            // Console.Error.WriteLine(string.Format("Compare Pellet: [{0},{1},{2}] - {3} ",pellet[0],pellet[1],pellet[2],currentValue));
+            // Console.Error.WriteLine(string.Format("Max: {0} => Compare Pellet: [{1},{2},{3}] - {4} ",maxValue,pellet[0],pellet[1],pellet[2],currentValue));
             if(currentValue > maxValue) {
                 maxValue = currentValue;
                 maxPellet =  pellet;
+                // Console.Error.WriteLine(string.Format("Max Pellet updated: [{0},{1}]-{2}", maxPellet[0], maxPellet[1], maxValue));
             }
         }
+        Pellets[pacMan[2]].Remove(maxPellet);
         
         return maxPellet;
     }
@@ -205,16 +195,4 @@ class Player
     static int distance(int[] pacMan, int[] pellet) {
         return Math.Abs(pellet[0]-pacMan[0]) + Math.Abs(pellet[1]-pacMan[1]);
     }
-
-    // static int[] getRandomLoc() {
-    //     var currentChar = '#';
-    //     var randLoc = new int[2];
-    //     while (currentChar == '#') {
-    //         randLoc[0] = rGen.Next(0, mapDimensions[0]);
-    //         randLoc[1] = rGen.Next(0, mapDimensions[1]);
-    //         currentChar = map[randLoc[1]][randLoc[0]];
-    //     }
-    //     return randLoc;
-    // }
-
 }
