263c5cc6197b0294ba47df477ac0eb4919f1ff30
Erik Butcher <butcer0@gmail.com>
2020-05-15 21:53:00 -0400

    Updated PacManAI - Not running, persist for reset

--- ConsoleApp_Sandbox/AI Bots/PacMan/PacManAI.cs
+++ ConsoleApp_Sandbox/AI Bots/PacMan/PacManAI.cs
@@ -12,6 +12,9 @@ class Player
 {
     const int SUPER_PELLET_VALUE = 7;
     const int NUM_PARTITIONS = 6;
+    const int STATUS_AVAILABLE = -1;
+    const int STATUS_BUSY = 0;
+    const int STATUS_COMPLETE = 1;
 
     static List<string> Map = new List<string>();
     static int[] PelletsSubParts;
@@ -20,8 +23,7 @@ class Player
     static Dictionary<int, int[]> PacMenTargets = new Dictionary<int, int[]>();
     static Dictionary<int, int[]> EnemyPacMen = new Dictionary<int, int[]>();
     static Dictionary<int, List<int[]>> Pellets = new Dictionary<int, List<int[]>>();
-    // static Dictionary<int, PARTITIONSTATUS> PartitionStatus = new Dictionary<int, PARTITIONSTATUS>();
-    static List<PARTITIONSTATUS> PartitionStatus = new List<PARTITIONSTATUS>();
+    static List<int> PartitionStatus = new List<int>();
 
 
     static void Main(string[] args)
@@ -54,13 +56,15 @@ class Player
         // and set all partitions to available
         for(var i = 0; i < 6; i++) {
             Pellets.Add(i, new List<int[]>());
-            PartitionStatus.Add(PARTITIONSTATUS.AVAILABLE);
+            PartitionStatus.Add(STATUS_AVAILABLE);
         }
 
         var initialized = false;
+        
         // game loop
         while (true)
         {
+            // Console.Error.WriteLine("Starting game loop");
 
             inputs = Console.ReadLine().Split(' ');
             int myScore = int.Parse(inputs[0]);
@@ -77,40 +81,53 @@ class Player
                 int speedTurnsLeft = int.Parse(inputs[5]); // unused in wood leagues
                 int abilityCooldown = int.Parse(inputs[6]); // unused in wood leagues
 
+                // Console.Error.WriteLine(string.Format("Parsed input: [{0},{1}] {2})", x, y, mine?"mine":"enemey"));
+
                 if(!initialized) {
                     if(mine) {
-                        PacMen.Add(pacId, new int[3]{x, y, (int)PARTITIONSTATUS.AVAILABLE});
+                        PacMen.Add(pacId, new int[3]{x, y, STATUS_AVAILABLE});
                         PacMenTargets.Add(pacId, null);
                     } else {
                         EnemyPacMen.Add(pacId, new int[2]{x, y});
                     }
                 } else {
-                    if(mine) {
-                        PacMen[pacId][0] = x;
-                        PacMen[pacId][1] = y;
-                    } else {
-                        EnemyPacMen[pacId][0] = x;
-                        EnemyPacMen[pacId][1] = y;
-                    }
+                    // Console.Error.WriteLine("Starting update PacMen location");
+                    // todo: reintroduce                   
+                    // if(mine) {
+                    //     PacMen[pacId][0] = x;
+                    //     PacMen[pacId][1] = y;
+                    // } else {
+                    //     EnemyPacMen[pacId][0] = x;
+                    //     EnemyPacMen[pacId][1] = y;
+                    // }
                 }
 
-                if(mine & PacMen[pacId][2] == (int)PARTITIONSTATUS.AVAILABLE) {
+                // Console.Error.WriteLine("Updated PacMen Locations");
+
+                if(mine & PacMen[pacId][2] == STATUS_AVAILABLE) {
+                    Console.Error.WriteLine(string.Format("Getting Partition for: {0}", pacId));
                     PacMen[pacId][2] = getAvailablePartition();           
                 }
 
+                // Console.Error.WriteLine("Updated PacMen Locations");
+
                 if(initialized) {
                     // drop target if hit
-                    if(PacMenTargets[pacId] != null && PacMen[pacId][0] == PacMenTargets[pacId][0] && PacMen[pacId][1] == PacMenTargets[pacId][1]) {
-                        PacMenTargets[pacId] = null;
-                    }
+                    // todo: reintroduce
+                    // if(PacMenTargets[pacId] != null && PacMen[pacId][0] == PacMenTargets[pacId][0] && PacMen[pacId][1] == PacMenTargets[pacId][1]) {
+                    //     PacMenTargets[pacId] = null;
+                    // }
 
                     // set target if empty
-                    if(PacMenTargets[pacId] == null) {
-                        PacMenTargets[pacId] = getClosestSubpartitionPellet(PacMen[pacId]);
-                    } 
+                    // todo: reintroduce
+                    // if(PacMenTargets[pacId] == null) {
+                    //     PacMenTargets[pacId] = getClosestSubpartitionPellet(PacMen[pacId]);
+                    // } 
                 }
             }
             
+            // Console.Error.WriteLine("Finished PacMen, Starting Pellets");
+
             int visiblePelletCount = int.Parse(Console.ReadLine()); // all pellets in sight
             if(!initialized) {
                 var pelletPartition = 0;
@@ -121,36 +138,46 @@ class Player
                     int y = int.Parse(inputs[1]);
                     int value = int.Parse(inputs[2]); // amount of points this pellet is worth
                     
-                    pelletPartition = PartitionMap[y,x];
+                    // todo: reintroduce
+                    // pelletPartition = PartitionMap[y,x];
                 
-                    Pellets[pelletPartition].Add(new int[3]{x, y, value == 1 ? 1 : SUPER_PELLET_VALUE});
+                    // todo: reintroduce;
+                    // Pellets[pelletPartition].Add(new int[3]{x, y, value == 1 ? 1 : SUPER_PELLET_VALUE});
                     // Console.Error.WriteLine(string.Format("Adding Pellet[{0}]: [{1},{2}]: {3}", pelletPartition, x, y, value));
                 }
             }
             initialized = true;
 
+            Console.Error.WriteLine("Initialized & Pellets Complete");
+
             // Console.WriteLine("MOVE 0 15 10"); // MOVE <pacId> <x> <y>
+            var action = string.Empty;
             
-            foreach(var pacManTarget in PacMenTargets) {
-                if(pacManTarget.Value != null) {
-                    Console.WriteLine(string.Format("MOVE {0} {1} {2}", pacManTarget.Key, pacManTarget.Value[0], pacManTarget.Value[1]));
-                }    
-            }
+            // todo: reintroduce
+            // foreach(var pacManTarget in PacMenTargets) {
+            //     if(pacManTarget.Value != null) {
+            //         action += string.Format("MOVE {0} {1} {2}|", pacManTarget.Key, pacManTarget.Value[0], pacManTarget.Value[1]);
+            //     }    
+            // }
+            action = string.IsNullOrEmpty(action)? "MOVE 0 15 10" : action.TrimEnd('|');
+            // Console.Error.WriteLine(string.Format("Sending Action: {0}", action));
+            Console.WriteLine(action);
         }
     }
 
     static int getAvailablePartition() {
         for(var i = 0; i < NUM_PARTITIONS; i++) {
-            if(PartitionStatus[i] == PARTITIONSTATUS.AVAILABLE) {
-                PartitionStatus[i] = PARTITIONSTATUS.BUSY;
+            if(PartitionStatus[i] == STATUS_AVAILABLE) {
+                PartitionStatus[i] = STATUS_BUSY;
                 return i;
             }
         }
-        return -1;
+        return STATUS_AVAILABLE;
     }
 
     static int[] getClosestSubpartitionPellet(int[] pacMan) {
-        if(pacMan[2] < (int)PARTITIONSTATUS.AVAILABLE) {
+        // Console.Error.WriteLine(string.Format("Getting Pellet Location: [{0},{1},{2}]", pacMan[0],pacMan[1],pacMan[2]));
+        if(pacMan[2] < STATUS_AVAILABLE) {
             return null;
         }
         
@@ -158,7 +185,9 @@ class Player
         var currentValue = int.MinValue;
         int[] maxPellet = null;
         foreach(var pellet in Pellets[pacMan[2]]) {
+            
             currentValue = getLocationValue(pacMan, pellet);
+            // Console.Error.WriteLine(string.Format("Compare Pellet: [{0},{1},{2}] - {3} ",pellet[0],pellet[1],pellet[2],currentValue));
             if(currentValue > maxValue) {
                 maxValue = currentValue;
                 maxPellet =  pellet;
@@ -188,10 +217,4 @@ class Player
     //     return randLoc;
     // }
 
-    enum PARTITIONSTATUS
-    {
-        AVAILABLE = -1,
-        BUSY = 0,
-        COMPLETE = 1
-    }
 }
